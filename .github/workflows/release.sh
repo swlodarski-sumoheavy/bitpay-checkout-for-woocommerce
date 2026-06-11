4ded6c2c1ee26646bc5cd1e2777f79ab9b5f9e75
Szymon Wlodarski <swlodarski@itx.com>
2026-06-11 15:02:48 +0200

    Update release workflow

--- .github/workflows/release.sh
+++ .github/workflows/release.sh
@@ -1,35 +1,37 @@
 #!/bin/bash
 
-if [[ -z "$SVN_USERNAME" ]]; then
-  echo "Set the SVN_USERNAME secret"
-  exit 1
-fi
+# if [[ -z "$SVN_USERNAME" ]]; then
+#   echo "Set the SVN_USERNAME secret"
+#   exit 1
+# fi
 
-if [[ -z "$SVN_PASSWORD" ]]; then
-  echo "Set the SVN_PASSWORD secret"
-  exit 1
-fi
+# if [[ -z "$SVN_PASSWORD" ]]; then
+#   echo "Set the SVN_PASSWORD secret"
+#   exit 1
+# fi
 
 VERSION=${GITHUB_REF#refs/tags/} # refs/tags/1.0.0 -> v1.0.0
 
 SVN_URL="https://plugins.svn.wordpress.org/${SLUG}/"
 SVN_DIR="${HOME}/svn-${SLUG}"
 
-svn checkout --quiet "$SVN_URL" "$SVN_DIR" > /dev/null
+echo "$SVN_URL" "$SVN_DIR"
+
+# svn checkout --quiet "$SVN_URL" "$SVN_DIR" > /dev/null
 
 if [[ -d "tags/$VERSION" ]]; then
   echo "ℹ︎ Version $VERSION of plugin $SLUG was already published";
-  exit
+  # exit
 fi
+echo "$SVN_DIR/tags/$VERSION"
+# mkdir $SVN_DIR/tags/$VERSION -p
 
-mkdir $SVN_DIR/tags/$VERSION -p
+# echo "Copy project to SVN"
+# cp -R $GITHUB_WORKSPACE/* $SVN_DIR/tags/$VERSION
 
-echo "Copy project to SVN"
-cp -R $GITHUB_WORKSPACE/* $SVN_DIR/tags/$VERSION
+# cd $SVN_DIR
+# echo "Add version to tags"
+# svn add tags/$VERSION
 
-cd $SVN_DIR
-echo "Add version to tags"
-svn add tags/$VERSION
-
-echo "Commit"
-svn commit -m "Tagging version $VERSION" --no-auth-cache --non-interactive --username "$SVN_USERNAME" --password "$SVN_PASSWORD"
+# echo "Commit"
+# svn commit -m "Tagging version $VERSION" --no-auth-cache --non-interactive --username "$SVN_USERNAME" --password "$SVN_PASSWORD"
