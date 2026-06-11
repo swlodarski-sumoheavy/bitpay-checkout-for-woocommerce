de05effce0b6d4798ecad479b6d8ed5f5bb64c87
Szymon Wlodarski <swlodarski@itx.com>
2026-06-11 15:21:05 +0200

    next test

--- .github/workflows/release.sh
+++ .github/workflows/release.sh
@@ -10,7 +10,8 @@
 #   exit 1
 # fi
 
-VERSION=${GITHUB_REF#refs/tags/} # refs/tags/1.0.0 -> v1.0.0
+VERSION=$1
+# VERSION=${GITHUB_REF#refs/tags/} # refs/tags/1.0.0 -> v1.0.0
 
 SVN_URL="https://plugins.svn.wordpress.org/${SLUG}/"
 SVN_DIR="${HOME}/svn-${SLUG}"
